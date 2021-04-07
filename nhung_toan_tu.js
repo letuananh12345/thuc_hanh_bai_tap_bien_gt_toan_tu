0ed42045fbcf21248bf1b9e306572c0dc2e5240c
letuananh12345 <letuananhmdc6789@gmail.com>
2021-04-07 14:23:52 +0700

    them

---
+++ nhung_toan_tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("the width");
+inputHeight = prompt("the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
